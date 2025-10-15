00a514b394acc81842cf3f70e858512bad0eb27b
codeclancy <128457307+codeclancy@users.noreply.github.com~>
2025-10-15 13:48:45 -0400

    Carousel WIP

---
+++ script.js
@@ -0,0 +1,28 @@
+const modal = document.querySelector('#modal');
+const openModal = document.querySelector('.open-button');
+const closeModal = document.querySelector('.close-button');
+
+openModal.addEventListener('click', () => {
+    modal.showModal()
+})
+
+closeModal.addEventListener('click', () => {
+    modal.close()
+})
+
+
+const buttons = document.querySelector("[data-carousel-button]")
+
+buttons.forEach(button => {
+    button.addEventListener("click", () =>{
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button.closest("[data-carousel]").querySelector('[data-slides]')
+        const activeSlide = slides.querySelector("[data-active]")
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+        if (newIndex < 0) newIndex = slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
+
+        slides.children[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
+    })
+})
