e6bd7e4b166b6ab9c0426e4668d179520ebc8557
codeclancy <128457307+codeclancy@users.noreply.github.com~>
2025-12-18 10:46:59 -0500

    FAQ accordion

--- script.js
+++ script.js
@@ -29,3 +29,27 @@ buttons.forEach(button => {
         delete activeSlide.dataset.active
     })
 })
+const accordion = document.querySelector(".accordion");
+
+if(accordion){
+accordion.addEventListener('click', (e) => {
+    const activePanel = e.target.closest(".accordion-panel");
+    if(!activePanel) return;
+    toggleAccordion(activePanel);
+});
+};
+
+function toggleAccordion(panelToActivate) {
+    const buttons = panelToActivate.parentElement.querySelectorAll("button");
+    const contents = panelToActivate.parentElement.querySelectorAll('.accordion-content');
+    console.log(buttons);
+    buttons.forEach((button) => {
+        button.setAttribute("aria-expanded", false);
+    });
+
+    contents.forEach((content) => {
+        content.setAttribute("aria-hidden", true);
+    });
+    panelToActivate.querySelector('button').setAttribute('aria-expanded', true);
+    panelToActivate.querySelector('.accordion-content').setAttribute('aria-hidden', false);
+}
